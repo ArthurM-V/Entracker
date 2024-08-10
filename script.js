7a33353c148489c4769d87ddac2b5aec7e998a22
Arthur Mesquita <arhurmesquita404@gmail.com>
2024-08-10 12:04:03 -0300

    design adjustments and button addition

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const themeBtn = document.querySelector(".material-symbols-outlined");
 const layout = document.querySelector(".layout");
 
+// dark theme activation
 function nightTime() {
   layout.classList.toggle("night-theme");
   document.body.classList.toggle("night-theme");
 }
 
 themeBtn.addEventListener("click", nightTime);
+
+// goal creation
+function addGoal() {}

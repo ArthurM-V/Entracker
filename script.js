abbd08a69a9f6faa9ee84d2e2f715570886f4fea
Arthur Mesquita <arhurmesquita404@gmail.com>
2024-08-18 14:39:52 -0300

    editing screen addition

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-const themeBtn = document.querySelector(".material-symbols-outlined");
-const layout = document.querySelector(".layout");
+// botões
+const themeBtn = document.querySelector(".lightbulb");
+const goalBtn = document.querySelector(".create-button");
+const cancelBtn = document.querySelector(".cancel");
 
-// dark theme activation
+// seções
+const layout = document.querySelector(".layout");
+const overlay = document.querySelector(".overlay");
+const editGoal = document.querySelector(".edit-goal");
+
+// dados
+
+// ativa tema escuro
 function nightTime() {
   layout.classList.toggle("night-theme");
   document.body.classList.toggle("night-theme");
@@ -9,5 +18,12 @@ function nightTime() {
 
 themeBtn.addEventListener("click", nightTime);
 
-// goal creation
+// lida com as metas
 function addGoal() {}
+function toggleEdit() {
+  overlay.classList.toggle("hidden");
+  editGoal.classList.toggle("hidden");
+}
+
+goalBtn.addEventListener("click", toggleEdit);
+cancelBtn.addEventListener("click", toggleEdit);
